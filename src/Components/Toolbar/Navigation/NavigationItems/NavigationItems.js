aa92e472ef4027180fd2a93c2f0851e94d483906
Rob <rob_cafferkey@hotmail.co.uk>
2020-08-05 11:41:41 +0100

    Setting up of navigation toolbar.

---
+++ src/Components/Toolbar/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem'
+import classes from '../../../../Css/NavigationItems.module.css';
+
+const navigationItems = (props) => (
+        <div className={classes.NavigationItems}>
+            <NavigationItem link="./">Add Receipt</NavigationItem>
+            <NavigationItem link="./">Filter</NavigationItem>
+        </div>
+);
+
+export default navigationItems;
--- src/Hoc/Layout/Layout.js
+++ src/Hoc/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Aux from '../Aux/Aux'
-import classes from '../../Css/Layout.module.Css';
+import classes from '../../Css/Layout.module.css';
+import Toolbar from '../../Components/Toolbar/Toolbar'
 
 class Layout extends Component {
     render () {
         return (
             <Aux>
+                <Toolbar />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
